83234418b9352c1c7528461d197fc39b43df1102
Bappa Adak <baapaadak@gmail.com>
2026-02-12 00:07:18 +0530

    Fix Vercel deployment issues

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,12 +1,14 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY ;
-
-
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, // Added this
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`, // Added this
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`, // Optional
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,   // Optional
 };
 
 export default requests;
